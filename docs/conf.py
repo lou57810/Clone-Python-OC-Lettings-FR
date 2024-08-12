03dfb77d3671e0a86662606e0a558b5f18fc30de
lou57810 <bernard.oswald@free.fr>
2024-08-12 23:19:25 +0200

    readthedocs.yaml update.

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,8 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../"))
+
 project = 'Clone_Python_OC-Lettings-FR'
 copyright = '2024, Ben_Oc'
 author = 'Ben_Oc'
