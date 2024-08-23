83c262a11c70e6213f4aa5d2bbd78b278b83f941
lou57810 <bernard.oswald@free.fr>
2024-08-23 15:31:57 +0200

    flake8

--- lettings/views.py
+++ lettings/views.py
@@ -6,17 +6,18 @@ from sentry_sdk import capture_message, capture_exception, set_tag
 def lettings_index(request):
 
     """ Retourne la page d'index."""
-    current_url = request.build_absolute_uri()
+    # current_url = request.build_absolute_uri()
     # print('current_url:', current_url)
     # Vérifier si l'URL est alphanumérique et correspond à l'URL attendue
 
-    """ Retourne une liste de locations. 
+    """ Retourne une liste de locations
     if current_url.isalnum() or current_url == 'http://127.0.0.1:8000/lettings/index/':
         lettings_list = Letting.objects.all()
         context = {'lettings_list': lettings_list}
         return render(request, 'lettings/index.html', context)
-    else:
-        return render(request, 'error404.html')"""
+        else:
+        return render(request, 'error404.html')
+    """
     lettings_list = Letting.objects.all()
     context = {'lettings_list': lettings_list}
     return render(request, 'lettings/index.html', context)
